041dec129b3256325b1491278c0868bb952c8430
HeBuDuMkA777 <87447478+HeBuDuMkA777@users.noreply.github.com>
2024-11-10 20:01:06 +0300

    выполнил третье задание

--- task_3.py
+++ task_3.py
@@ -17,3 +17,12 @@ print('Задача 3. Аналог Steam')
 # Прошло 3 сек. Скачано 81 из 123 Мб (66%)
 # Прошло 4 сек. Скачано 108 из 123 Мб (88%)
 # Прошло 5 сек. Скачано 123 из 123 Мб (100%)
+
+import math
+
+size = int(input("Укажите размер файла для скачивания: "))
+speed = int(input("Какова скорость вашего соединения: "))
+
+for time in range(1, math.ceil(size / speed)):
+    print(f' Прошло {time} сек. Скачано {speed * time} из {size} Мб ({math.ceil(((speed * time) / size) * 100)}%)')
+print(f' Прошло {math.ceil(size / speed)} сек. Скачано {size} из {size} Мб (100%)')
